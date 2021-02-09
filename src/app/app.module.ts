fa6f0bffac0b3acd5573506b4e5efdf019f7dda8
Emanuel Contigliani <emanuelconti@hotmail.com>
2021-02-09 20:01:13 -0300

    Obtener productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {HttpClientModule} from '@angular/common/http'
 //Componentes
 import { AppComponent } from './app.component';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
@@ -23,7 +22,8 @@ import { ListarProductosComponent } from './components/listar-productos/listar-p
     AppRoutingModule,
     ReactiveFormsModule,
     ToastrModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
+ url: string = 'http://localhost:4000/api/productos/';
+    
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getProductos() : Observable<any> {
+    return this.http.get(this.url)
+  }
 }
